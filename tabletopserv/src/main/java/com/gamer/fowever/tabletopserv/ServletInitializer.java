e22ef9ea2adb41bc7d36805c608e8809ba56eab2
wolfman <pshook456@gmai.com>
2026-08-25 23:34:15 -0500

    back end service frame work

---
+++ tabletopserv/src/main/java/com/gamer/fowever/tabletopserv/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gamer.fowever.tabletopserv;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TabletopservApplication.class);
+    }
+
+}
---
+++ tabletopserv/src/main/java/com/gamer/fowever/tabletopserv/TabletopservApplication.java
@@ -0,0 +1,13 @@
+package com.gamer.fowever.tabletopserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabletopservApplication.class, args);
+    }
+
+}
